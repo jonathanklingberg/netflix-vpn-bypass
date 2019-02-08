e19cce04ddd1f892b61451f931e3397c30d1e163
Jonathan Klingberg <jonathan.klingberg@gmail.com>
2019-02-08 17:55:00 +0200

    Added support for mapping multiple domains to multiple interfaces, not only Netflix related domains. This can be used for mapping bbc to a UK VPN client at the same time as Netflix is mapped to US and plex.tv is mapped to WAN etc. To achieve this I however needed to use associative arrays which is available in Bash 4 which was installed using Entware. This facilitates mapping multiple domains to different interfaces. I’ve also updated the fwmarks, breaking backwards-compatibility. Also note that I haven’t updated the README according to these modifications either.

--- IPSET_Netflix_Domains.sh
+++ IPSET_Netflix_Domains.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/tmp/mnt/sda1/entware/bin/bash
 ####################################################################################################
 # Script: IPSET_Netflix_Domains.sh
 # Version 1.0
@@ -46,40 +46,53 @@ error_exit() {
 main() {
     lock "$PROGNAME" || error_exit "Exiting $PROGNAME. Only one instance of $PROGNAME can run at one time."
 
+###
+set_domain_array () {
+  declare -gA DOMAINS
+  DOMAINS[WAN]='/plex.tv'
+  # US
+  DOMAINS[OVPNC2]='/amazonaws.com/netflix.com/nflxext.com/nflximg.net/nflxso.net/nflxvideo.net'
+  # UK
+  DOMAINS[OVPNC4]='/bbc.gscontxt.net/bbci.co.uk/bbc.co.uk/bbc.com/bbctvapps.co.uk/bbciplayer.metafaq.com/xhst.bbci/bbci.co.uk/bbc.co.uk/bbcfmt.hs.llnwd.net/bbc.net.uk'
+  # SE
+  DOMAINS[OVPNC5]='/akamaized.net/svt.demdex.net/play.svt.se/svt.d3.sc.omtrdc.net/svtstatic.se'
+}
+
 ### Define interface/bitmask to route traffic to below
-set_fwmark_parms () {
-    FWMARK_WAN="0x8000/0x8000"
-    FWMARK_OVPNC1="0x1000/0x1000"
-    FWMARK_OVPNC2="0x2000/0x2000"
-    FWMARK_OVPNC3="0x4000/0x4000"
-    FWMARK_OVPNC4="0x7000/0x7000"
-    FWMARK_OVPNC5="0x3000/0x3000"
+set_fwmark_array () {
+    declare -gA FWMARK
+    FWMARK[WAN]="0x8000/0x8000"
+    FWMARK[OVPNC1]="0x1000/0x1000"
+    FWMARK[OVPNC2]="0x2000/0x2000"
+    FWMARK[OVPNC3]="0x3000/0x3000"
+    FWMARK[OVPNC4]="0x4000/0x4000"
+    FWMARK[OVPNC5]="0x5000/0x5000"
 }
 
 create_fwmarks () {
 # WAN
-    ip rule del fwmark "$FWMARK_WAN" > /dev/null 2>&1
-    ip rule add from 0/0 fwmark "$FWMARK_WAN" table 254 prio 9990
-    
+    ip rule del fwmark "${FWMARK[WAN]}" > /dev/null 2>&1
+    ip rule add from 0/0 fwmark "${FWMARK[WAN]}" table 254 prio 9990
+
 #VPN Client 1
-    ip rule del fwmark "$FWMARK_OVPNC1" > /dev/null 2>&1
-    ip rule add from 0/0 fwmark "$FWMARK_OVPNC1" table 111 prio 9995
+    ip rule del fwmark "${FWMARK[OVPNC1]}" > /dev/null 2>&1
+    ip rule add from 0/0 fwmark "${FWMARK[OVPNC1]}" table 111 prio 9995
 
 #VPN Client 2
-    ip rule del fwmark "$FWMARK_OVPNC2" > /dev/null 2>&1
-    ip rule add from 0/0 fwmark "$FWMARK_OVPNC2" table 112 prio 9994
+    ip rule del fwmark "${FWMARK[OVPNC2]}" > /dev/null 2>&1
+    ip rule add from 0/0 fwmark "${FWMARK[OVPNC2]}" table 112 prio 9994
 
 #VPN Client 3
-    ip rule del fwmark "$FWMARK_OVPNC3" > /dev/null 2>&1
-    ip rule add from 0/0 fwmark "$FWMARK_OVPNC3" table 113 prio 9993
+    ip rule del fwmark "${FWMARK[OVPNC3]}" > /dev/null 2>&1
+    ip rule add from 0/0 fwmark "${FWMARK[OVPNC3]}" table 113 prio 9993
 
 #VPN Client 4
-    ip rule del fwmark "$FWMARK_OVPNC4" > /dev/null 2>&1
-    ip rule add from 0/0 fwmark "$FWMARK_OVPNC4" table 114 prio 9992
+    ip rule del fwmark "${FWMARK[OVPNC4]}" > /dev/null 2>&1
+    ip rule add from 0/0 fwmark "${FWMARK[OVPNC4]}" table 114 prio 9992
 
 #VPN Client 5
-    ip rule del fwmark "$FWMARK_OVPNC5" > /dev/null 2>&1
-    ip rule add from 0/0 fwmark "$FWMARK_OVPNC5" table 115 prio 9991
+    ip rule del fwmark "${FWMARK[OVPNC5]}" > /dev/null 2>&1
+    ip rule add from 0/0 fwmark "${FWMARK[OVPNC5]}" table 115 prio 9991
 
     ip route flush cache
 }
@@ -136,59 +149,71 @@ Chk_Entware () {
 }
 
 # check if /jffs/configs/dnsmasq.conf.add contains entry for Netflix domains
+# takes the interface as parameter
 check_dnsmasq () {
+    IFACE="$1"
+    IFACE_DOMAINS=${DOMAINS[$IFACE]}
     if [ -s /jffs/configs/dnsmasq.conf.add ]; then  # dnsmasq.conf.add file exists
-        if [ "$(grep -c "ipset=/amazonaws.com/netflix.com/nflxext.com/nflximg.net/nflxso.net/nflxvideo.net/x3mRouting_NETFLIX_DNSMASQ" "/jffs/configs/dnsmasq.conf.add")" -eq "0" ]; then  # see if line exists for x3mRouting_NETFLIX_DNSMASQ
-            printf "ipset=/amazonaws.com/netflix.com/nflxext.com/nflximg.net/nflxso.net/nflxvideo.net/x3mRouting_NETFLIX_DNSMASQ\n" >> /jffs/configs/dnsmasq.conf.add # add NETFLIX entry to dnsmasq.conf.add
+        if [ "$(grep -c "ipset=$IFACE_DOMAINS/${IFACE}_DNSMASQ" "/jffs/configs/dnsmasq.conf.add")" -eq "0" ]; then  # see if line exists for OVPNC1_DNSMASQ
+            printf "ipset=$IFACE_DOMAINS/${IFACE}_DNSMASQ\n" >> /jffs/configs/dnsmasq.conf.add # add NETFLIX entry to dnsmasq.conf.add
             service restart_dnsmasq > /dev/null 2>&1
         fi
     else
-        printf "ipset=/amazonaws.com/netflix.com/nflxext.com/nflximg.net/nflxso.net/nflxvideo.net/x3mRouting_NETFLIX_DNSMASQ\n" > /jffs/configs/dnsmasq.conf.add # dnsmasq.conf.add does not exist, create dnsmasq.conf.add
+        printf "ipset=$IFACE_DOMAINS/${IFACE}_DNSMASQ\n" > /jffs/configs/dnsmasq.conf.add # dnsmasq.conf.add does not exist, create dnsmasq.conf.add
         service restart_dnsmasq > /dev/null 2>&1
     fi
 }
 
 check_ipset_list () {
-    if [ "$(ipset list -n x3mRouting_NETFLIX_DNSMASQ 2>/dev/null)" != "x3mRouting_NETFLIX_DNSMASQ" ]; then #does NETFLIX ipset list exist?
-        if [ -s /opt/tmp/x3mRouting_NETFLIX_DNSMASQ ]; then # does x3mRouting_NETFLIX_DNSMASQ ipset restore file exist?
-            ipset restore -! < /opt/tmp/x3mRouting_NETFLIX_DNSMASQ   # Restore ipset list if restore file exists at /opt/tmp/x3mRouting_NETFLIX_DNSMASQ
+    IFACE="$1"
+    if [ "$(ipset list -n ${IFACE}_DNSMASQ 2>/dev/null)" != "${IFACE}_DNSMASQ" ]; then #does NETFLIX ipset list exist?
+        if [ -s "/opt/tmp/${IFACE}_DNSMASQ" ]; then # does OVPNC1_DNSMASQ ipset restore file exist?
+            ipset restore -! < /opt/tmp/${IFACE}_DNSMASQ   # Restore ipset list if restore file exists at /opt/tmp/OVPNC1_DNSMASQ
         else
-            ipset create x3mRouting_NETFLIX_DNSMASQ hash:net family inet hashsize 1024 maxelem 65536  # No restore file, so create x3mRouting_NETFLIX_DNSMASQ ipset list from scratch
+            ipset create ${IFACE}_DNSMASQ hash:net family inet hashsize 1024 maxelem 65536  # No restore file, so create OVPNC1_DNSMASQ ipset list from scratch
         fi
     fi
 }
 
 # if ipset list NETFLIX is older than 24 hours, save the current ipset list to disk
-check_NETFLIX_restore_file_age () {
-    if [ -s /opt/tmp/x3mRouting_NETFLIX_DNSMASQ ]; then
-        if [ "$(find /opt/tmp/x3mRouting_NETFLIX_DNSMASQ -name x3mRouting_NETFLIX_DNSMASQ -mtime +1 -print /dev/null 2>&1)" = "/opt/tmp/x3mRouting_NETFLIX_DNSMASQ" ] ; then
-            ipset save x3mRouting_NETFLIX_DNSMASQ > /opt/tmp/x3mRouting_NETFLIX_DNSMASQ
+check_ipset_restore_file_age () {
+    IFACE="$1"
+    if [ -s "/opt/tmp/${IFACE}_DNSMASQ" ]; then
+        if [ "$(find /opt/tmp/${IFACE}_DNSMASQ -name ${IFACE}_DNSMASQ -mtime +1 -print /dev/null 2>&1)" = "/opt/tmp/${IFACE}_DNSMASQ" ] ; then
+            ipset save ${IFACE}_DNSMASQ > /opt/tmp/${IFACE}_DNSMASQ
         fi
     fi
 }
 
 # If cronjob to back up the NETFLIX ipset list every 24 hours @ 2:00 AM does not exist, then create it
 check_cron_job () {
-    cru l | grep x3mRouting_NETFLIX_DNSMASQ_ipset_list
+    IFACE="$1"
+    cru l | grep ${IFACE}_DNSMASQ_ipset_list
     if [ "$?" = "1" ]; then  # no cronjob entry found, create it
-        cru a x3mRouting_NETFLIX_DNSMASQ "0 2 * * * ipset save x3mRouting_NETFLIX_DNSMASQ > /opt/tmp/x3mRouting_NETFLIX_DNSMASQ"
+        cru a ${IFACE}_DNSMASQ "0 2 * * * ipset save ${IFACE}_DNSMASQ > /opt/tmp/OVPNC1_DNSMASQ"
     fi
 }
 
 # Route Netflix to WAN
 create_routing_rules () {
-    iptables -t mangle -D PREROUTING -i br0 -m set --match-set x3mRouting_NETFLIX_DNSMASQ dst -j MARK --set-mark "$FWMARK_WAN" > /dev/null 2>&1
-    iptables -t mangle -A PREROUTING -i br0 -m set --match-set x3mRouting_NETFLIX_DNSMASQ dst -j MARK --set-mark "$FWMARK_WAN"
+    IFACE="$1"
+    iptables -t mangle -D PREROUTING -i br0 -m set --match-set ${IFACE}_DNSMASQ dst -j MARK --set-mark "${FWMARK[$IFACE]}" > /dev/null 2>&1
+    iptables -t mangle -A PREROUTING -i br0 -m set --match-set ${IFACE}_DNSMASQ dst -j MARK --set-mark "${FWMARK[$IFACE]}"
 }
 
-set_fwmark_parms
+set_domain_array
+set_fwmark_array
 create_fwmarks
 Chk_Entware
-check_dnsmasq
-check_ipset_list
-check_NETFLIX_restore_file_age
-check_cron_job
-create_routing_rules
+# initiate the loop
+for IFACE in "${!DOMAINS[@]}"; do
+  echo "Configuring $IFACE --- ${DOMAINS[$IFACE]}"
+  check_dnsmasq $IFACE
+  check_ipset_list $IFACE
+  check_ipset_restore_file_age $IFACE
+  check_cron_job $IFACE
+  create_routing_rules $IFACE
+done
 
 logger -t "($(basename "$0"))" $$ Completed Script Execution
 }
